6aefd1afc86122dd79e181a83c7645f31d08fcbe
isqas <119613421+isqas@users.noreply.github.com>
2022-12-01 23:17:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const CONFIG = {
     introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Lạc vào tim thế này
+    Nên hãy chịu trách nhiệm đi`,
     btnIntro: 'hihi',
     title: 'Bây giờ em có hai sự lựa chọn 😙',
     desc: 'Nhưng lựa chọn này sẽ không cho em nói dối ',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Hẹn công chúa thứ 7 gặp em nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.google.com.vn/search?tbm=isch&q=tr%C3%A1i+tim+to+%C4%91%C3%B9ng' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/phuonganh41' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
